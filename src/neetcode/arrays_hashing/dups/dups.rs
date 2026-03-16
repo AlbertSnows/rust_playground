ed0314d2706d8c08372435906973a29f4867fc74
Albert Snow <ajsnow2012@gmail.com>
2026-03-15 22:19:57 -0500

    adding more stuff to get tests working

--- src/neetcode/arrays_hashing/dups/dups.rs
+++ src/neetcode/arrays_hashing/dups/dups.rs
@@ -1,9 +1,20 @@
-impl Solution {
-    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
-        // nums = [1, 1, 2, 3]
-        // nums = [3, 4, 5]
-        // hashset = {1, 2, 3}
+pub fn add_if_not_exists(hashset: &mut std::collections::HashSet<i32>, num: i32) -> bool {
+    if hashset.contains(&num) {
+        return true;
     }
+    hashset.insert(num);
+    false
+}
+
+pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+    let mut hashset = std::collections::HashSet::new();
+    let is_dup = |num: &i32| add_if_not_exists(&mut hashset, *num);
+    // drop(is_dup); if you drop, you have to redefine the clojure
+    // hashset.insert(99); illegal, bc is_dup borrows hashset
+    nums.iter().any(is_dup)
+    // nums = [1, 1, 2, 3]
+    // nums = [3, 4, 5]
+    // hashset = {1, 2, 3}
 }
 
 #[cfg(test)]
@@ -12,11 +23,11 @@ mod tests {
 
     #[test]
     fn basic() {
-        assert_eq!(two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
+        assert_eq!(contains_duplicate(vec![1, 2, 3]), false);
     }
 
     #[test]
     fn duplicate_values() {
-        assert_eq!(two_sum(vec![3, 3], 6), vec![0, 1]);
+        assert_eq!(contains_duplicate(vec![3, 3]), true);
     }
 }
