09530a6a1cbbc573580ce21c219902639baed63b
Albert Snow <ajsnow2012@gmail.com>
2026-04-02 20:04:10 -0500

    product except self

--- src/neetcode/arrays_hashing/product_except_self/mod.rs
+++ src/neetcode/arrays_hashing/product_except_self/mod.rs
@@ -1,5 +1,25 @@
+// Given an integer array, return an array where each element is the product of all other elements (no division allowed).
+
+// ## Example
+// Input: nums = [1,2,4,3,4]
+// Output: [96, 48, 24, 32, 24]
 pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
-    Default::default()
+    let num_zeros = nums.iter().filter(|&num| num == &0).count();
+    if num_zeros > 1 {
+        return vec![0; nums.len()];
+    } else if num_zeros == 1 {
+        let zero_index = nums.iter().position(|&num| num == 0).unwrap();
+        let multiplication_sum: i32 = nums.iter().filter(|&&n| n != 0).product();
+        let mut result = vec![0; nums.len()];
+        result[zero_index] = multiplication_sum;
+        return result;
+    }
+    let multiplication_sum: i32 = nums.iter().product();
+    let product_of_other_elements = nums
+        .iter()
+        .map(|&num| multiplication_sum / num)
+        .collect::<Vec<i32>>();
+    product_of_other_elements
 }
 
 #[cfg(test)]
@@ -13,6 +33,9 @@ mod tests {
 
     #[test]
     fn with_zero() {
-        assert_eq!(product_except_self(vec![-1, 1, 0, -3, 3]), vec![0, 0, 9, 0, 0]);
+        assert_eq!(
+            product_except_self(vec![-1, 1, 0, -3, 3]),
+            vec![0, 0, 9, 0, 0]
+        );
     }
 }
