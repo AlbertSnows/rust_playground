60b78baffa1cc42c445b47d104b6a6153905e055
Albert Snow <ajsnow2012@gmail.com>
2026-04-11 17:59:07 -0500

    awful implementation of three_sum

--- src/neetcode/two_pointers/three_sum/mod.rs
+++ src/neetcode/two_pointers/three_sum/mod.rs
@@ -6,53 +6,57 @@
 // Input: nums = [-1,0,1,2,-1,-4]
 // Output: [[-1,-1,2],[-1,0,1]]
 
-use crate::{
-    common::maps::{freq_map, unroll_freq_map},
-    neetcode::arrays_hashing::two_sum::mod_multiple_solution_sorted::two_sum,
-};
+use crate::common::maps::{freq_map, unroll_freq_map};
+use crate::neetcode::arrays_hashing::two_sum::mod_multiple_solution_sorted::two_sum;
 use itertools::Itertools;
-use std::{
-    collections::{HashMap, HashSet},
-    hash::Hash,
-};
+use std::collections::{HashMap, HashSet};
 
-fn get_three_sum_for_num(
-    num_freq: &HashMap<&i32, usize>,
-) -> impl Fn(i32) -> Vec<HashMap<i32, usize>> {
+fn get_three_sum_for_num<'a>(
+    num_freq: &'a HashMap<&'a i32, usize>,
+) -> impl Fn(i32) -> Vec<HashMap<i32, usize>> + 'a {
     move |num| {
         let mut two_sum_freq = num_freq.clone();
         *two_sum_freq.get_mut(&num).unwrap() -= 1;
         let two_sum_list = unroll_freq_map(&two_sum_freq);
+        // sorted_two_sum_list carries original indices so two_sum can return them,
+        // letting us look up values back in two_sum_list
         let sorted_two_sum_list = two_sum_list
             .iter()
             .enumerate()
-            .sorted_by_key(|(_, num): &(usize, &i32)| *num)
+            .sorted_by_key(|(_, num)| *num)
             .map(|(index, &num)| (index, num))
             .collect::<Vec<_>>();
-        let two_sum_outcome = two_sum(&sorted_two_sum_list, num);
+        let two_sum_outcome = two_sum(&sorted_two_sum_list, -num);
         two_sum_outcome
             .into_iter()
-            .map(|two_sum_solution: HashSet<usize>| {
-                let mut triplet_freq: HashMap<i32, usize> = two_sum_solution
-                    .into_iter()
-                    .map(|index| sorted_two_sum_list[index].1)
-                    .fold(HashMap::new(), |mut map, val| {
-                        *map.entry(val).or_insert(0) += 1;
-                        map
-                    });
-                *triplet_freq.entry(num).or_insert(0) += 1;
-                triplet_freq
+            .filter_map(|two_sum_solution: HashSet<usize>| {
+                // indices are original positions in two_sum_list (pre-sort)
+                let pair_values: Vec<i32> = two_sum_solution
+                    .iter()
+                    .map(|&original_index| two_sum_list[original_index])
+                    .collect();
+                // only keep triplet when num is the minimum value, so each triplet is generated once
+                if pair_values.iter().all(|&val| num <= val) {
+                    let mut triplet_freq =
+                        pair_values
+                            .into_iter()
+                            .fold(HashMap::new(), |mut map, val| {
+                                *map.entry(val).or_insert(0) += 1;
+                                map
+                            });
+                    *triplet_freq.entry(num).or_insert(0) += 1;
+                    Some(triplet_freq)
+                } else {
+                    None
+                }
             })
             .collect()
     }
 }
 
-// -1, 0, 1, 2, -1, 2, -4
-// -4 -1 -1 0 1 2 2
-// -4 1, -1 2, 0 1, 1 1, 2 2
 pub fn three_sum(nums: Vec<i32>) -> Vec<HashMap<i32, usize>> {
-    let unique_nums = nums.into_iter().collect::<std::collections::HashSet<_>>();
-    let num_freq = freq_map(unique_nums.iter());
+    let num_freq = freq_map(nums.iter());
+    let unique_nums: HashSet<i32> = nums.iter().copied().collect();
     unique_nums
         .iter()
         .copied()
@@ -66,13 +70,14 @@ mod tests {
 
     #[test]
     fn basic() {
-        let mut result = three_sum(vec![-1, 0, 1, 2, -1, -4]);
-        result.sort();
-        assert_eq!(result, vec![vec![-1, -1, 2], vec![-1, 0, 1]]);
+        let result = three_sum(vec![-1, 0, 1, 2, -1, -4]);
+        assert!(result.contains(&HashMap::from([(-1, 2), (2, 1)])));
+        assert!(result.contains(&HashMap::from([(-1, 1), (0, 1), (1, 1)])));
+        assert_eq!(result.len(), 2);
     }
 
     #[test]
     fn no_solution() {
-        assert_eq!(three_sum(vec![0, 1, 1]), vec![] as Vec<Vec<i32>>);
+        assert_eq!(three_sum(vec![0, 1, 1]), vec![]);
     }
 }
